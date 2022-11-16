b728e25ee5fdad67c1e3c11f6ea6f05784768d65
Jonithan42 <yonipardo4@gmail.com>
2022-11-15 21:01:09 -0300

    features: all pages created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["dist/index.html"],
   theme: {
+    screens: {
+      sm: '360px',
+      md: '768px',
+      lg: '1440px',
+    },
     extend: {
+      fontFamily: {
+        raleway: "'Raleway', 'sans-serif'",
+      }
     },
   },
   plugins: [],
